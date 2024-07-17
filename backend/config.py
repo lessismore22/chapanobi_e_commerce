b1a80ed18da7ffdd1821be02bc5c3679896d2678
lessismore22 <charlesigbe0@gmail.com>
2024-07-17 13:54:04 +0100

    backup

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
+import os
 from decouple import config
 
 class Config:
-    SECRET_KEY = config('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
